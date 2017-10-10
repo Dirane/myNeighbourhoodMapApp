5d60155681a04c5aad859d93f98c3b586f0c854e
dirane <diranengala@gmail.com>
2017-10-10 15:50:06 +0100

    Debugging and making changes to the function to recognize place ID of Buea

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Project 4: Build a Neighbourhood Map
 
 ## Introduction
-This application shows the locations I am interested in marked down on the map in Buea.
-When one of the location is clicked, a popup window shows additional info about the location from foursquare.
+This application shows the locations I am interested in marked down on the map, the locations are found in Buea Cameroon.
+When one of the location is clicked, a popup window shows additional info(recent comment) about the location gotten from foursquare.
 
 ## How to run this program
-First you need to have internet connection
+First you need to have a good internet connection
 Download the project files and open the file 'myNeighbourhoodMap.html' with your suitable browser.
 
 ### Project comments and Credits
--- js/myNeighbourhoodMap.js
+++ js/myNeighbourhoodMap.js
@@ -1,16 +1,15 @@
-// Location Class completely builds everything needed for each location marker.
-var Location = function(title, lng, lat, venueId) {
+// Place Class completely builds everything needed for each Place marker.
+var Place = function(title, lng, lat, venueId) {
 	var self = this;
 	this.title = title;
 	this.lng = lng;
 	this.lat = lat;
 	this.venueId = venueId;
 
-// getConetent function retrieves 5 most recent tips from foursquare for the marker location.
+// getConetent function retrieves 5 most recent tips from foursquare for the marker Place.
 	this.getContent = function() {
 		var topTips = [];
 		var venueUrl = 'https://api.foursquare.com/v2/venues/' + self.venueId + '/tips?sort=recent&limit=5&v=20131124&client_id=OIMLANYCX4OK0FAVBFVAYNRXLMNKWACXQCUPIC2XW34PTPLE&client_secret=5TF5ZFIMJHMT3ECQEH0YCVQGLNDD2TZBYFRZY2UK1LLF41YM';
-		//var venueUrl = 'https://api.foursquare.com/v2/venues/' + self.venueId + '/tips?sort=recent&limit=5&v=20150609&client_id=4EPS21I4V4MVCYXWDT4QNZZG1JETWZ2LIJMYQ34FNBWZ1RMV&client_secret=U3P1XLU204VMYO4BHGIWPDOY130Z1AFTT1OQTI2TY0HW0T43';
 
 		$.getJSON(venueUrl,
 			function(data) {
@@ -22,7 +21,7 @@ var Location = function(title, lng, lat, venueId) {
 
 				self.content = '<h2>' + self.title + '</h2>' + '<h3>5 Most Recent Comments</h3>' + '<ol class="tips">' + topTips.join('') + '</ol>';
 			}).fail(function(jqXHR, textStatus, errorThrown) {
-				self.content = '<h2>' + self.title + '</h2>' + '<h3>5 Most Recent Comments</h3>' + '<h4>Oops. There was a problem retrieving this location\'s comments.</h4>';				
+				self.content = '<h2>' + self.title + '</h2>' + '<h3>5 Most Recent Comments</h3>' + '<h4>Oops. There was a problem retrieving this Place\'s comments.</h4>';				
 				console.log('getJSON request failed! ' + textStatus);
 			});
 		}();
@@ -37,48 +36,48 @@ var Location = function(title, lng, lat, venueId) {
 			icon: self.icon
 		});
 
-		// Opens the info window for the location marker.
+		// Opens the info window for the Place marker.
 		this.openInfowindow = function() {
-			for (var i=0; i < locationsModel.locations.length; i++) {
-   			locationsModel.locations[i].infowindow.close();
+			for (var i=0; i < placesModel.places.length; i++) {
+   			placesModel.places[i].infowindow.close();
   		}
 			map.panTo(self.marker.getPosition())
 			self.infowindow.setContent(self.content);
 			self.infowindow.open(map,self.marker);
 		};
 
-		// Assigns a click event listener to the marker to open the info window.
+		// A click event listener to enable the marker open the info window.
 		this.addListener = google.maps.event.addListener(self.marker,'click', (this.openInfowindow));
 	};
 
-	// Contains all the locations and search function.
-	var locationsModel = {
+	// Contains all the places and search function.
+	var placesModel = {
 
-		locations:[
-		new Location('University Of Buea', 4.161446, 9.290732, '4cdd6918d4ecb1f701298548'),
-		//new Location('University Of Buea', 4.161446, 9.290732, 'ChIJQzTKNpIxYRARd0ST6rDHtr4'),
-		new Location('Chariot Hotel Buea', 4.157975, 0.296515, 'ChIJcT94s5QxYRARQCK3Yglrqhc'),
-		new Location('ActivSpaces Buea', 4.153117, 9.255252, 'ChIJrz8KIz8yYRARKgess8WF5Vo'),
-		new Location('Fakoship Plaza', 4.154326, 9.234717, 'ChIJk6byWl8yYRAR7saV6SkTgUc'),
-		new Location('Rhema Restaurant', 4.151131, 9.250948, 'ChIJOemdgUYyYRAR58rV6_8JtCs'),
-		new Location('Molyko Omnisport Stadium', 4.162015, 9.279930, 'ChIJ3YySefUxYRARhK4n8hO_1dE'),
-		new Location('Malingo Street', 4.1588855, 9.292790, 'ChIJMUinyOwxYRAR1ygcejjetGk'),
-		new Location('IYA Restaurant', 4.153185, 9.244418, 'ChIJEXb2WFsyYRARsJ2aAaW3XbA'),
-		new Location('Eta Palace Hotel', 4.151576, 9.296810, 'ChIJZyCG6ZYxYRARnp1J8eLin8k'),
-		new Location('Mount Cameroon', 4.224752, 9.213144, 'ChIJEcUaSt_SZhARp1XRcl66rfc'),
-		new Location('Sandpit Road', 4.146506, 9.282099, 'ChIJt2TEhSUyYRARAjd1MrMJVnA'),
+		places:[
+		//new Place('University Of Buea', 4.161446, 9.290732, '4cdd6918d4ecb1f701298548'),
+		new Place('University Of Buea', 4.161446, 9.290732, 'ChIJQzTKNpIxYRARd0ST6rDHtr4'),
+		new Place('Chariot Hotel Buea', 4.157975, 0.296515, 'ChIJcT94s5QxYRARQCK3Yglrqhc'),
+		new Place('ActivSpaces Buea', 4.153117, 9.255252, 'ChIJrz8KIz8yYRARKgess8WF5Vo'),
+		new Place('Fakoship Plaza', 4.154326, 9.234717, 'ChIJk6byWl8yYRAR7saV6SkTgUc'),
+		new Place('Rhema Restaurant', 4.151131, 9.250948, 'ChIJOemdgUYyYRAR58rV6_8JtCs'),
+		new Place('Molyko Omnisport Stadium', 4.162015, 9.279930, 'ChIJ3YySefUxYRARhK4n8hO_1dE'),
+		new Place('Malingo Street', 4.1588855, 9.292790, 'ChIJMUinyOwxYRAR1ygcejjetGk'),
+		new Place('IYA Restaurant', 4.153185, 9.244418, 'ChIJEXb2WFsyYRARsJ2aAaW3XbA'),
+		new Place('Eta Palace Hotel', 4.151576, 9.296810, 'ChIJZyCG6ZYxYRARnp1J8eLin8k'),
+		new Place('Mount Cameroon', 4.224752, 9.213144, 'ChIJEcUaSt_SZhARp1XRcl66rfc'),
+		new Place('Sandpit Road', 4.146506, 9.282099, 'ChIJt2TEhSUyYRARAjd1MrMJVnA'),
 		],
 		query: ko.observable(''),
 	};
 
 
-	// Search function for filtering through the list of locations based on the name of the location.
-	locationsModel.search = ko.dependentObservable(function() {
+	// Search function for filtering through the list of places based on the name of the Place.
+	placesModel.search = ko.dependentObservable(function() {
 		var self = this;
 		var search = this.query().toLowerCase();
-		return ko.utils.arrayFilter(self.locations, function(location) {
-			return location.title.toLowerCase().indexOf(search) >= 0;
+		return ko.utils.arrayFilter(self.places, function(Place) {
+			return Place.title.toLowerCase().indexOf(search) >= 0;a
 		});
-	}, locationsModel);
+	}, placesModel);
 
-	ko.applyBindings(locationsModel);
+	ko.applyBindings(placesModel);
